7f30a23ec0df74b53215576cd6bba7b5fae6f519
cassiegUZH <118742554+cassiegUZH@users.noreply.github.com>
2022-11-29 23:27:40 +0100

    Update spatial_analysis.R

--- spatial_analysis.R
+++ spatial_analysis.R
@@ -42,3 +42,49 @@ mantel(geodist ~ cultdist,nperm = 10000, mrank = T)
 # with y ~ x1 + x2, you calculate correlation between 
 # y and x1 controlling for x2 
 mantel(geodist ~ cultdist + gendist, nperm = 10000, mrank = T)
+
+
+## Code for preparation of real data
+
+  d.real <- read.csv("d.tools3.csv", sep = ";")
+  d.genes <- read.csv("d.genes3.csv", sep = ";")
+
+# Geographical distance
+  m.geo <- distinct(d.real[, c(1,4,5)], population, .keep_all = T)
+  rownames(m.geo) <- m.geo$population
+  m.geo <- m.geo[,-1]
+  geodist <- vegdist(m.geo, method = "euclidean", diag=F, upper=F)
+  geodist
+
+# cultural distance
+  m.cult <- distinct(d.real[, c(1,6,11)], population, tool, .keep_all = T)
+  m.cult <- tidyr::spread(m.cult, tool, use)
+  rownames(m.cult) <- m.cult$population
+  m.cult <- m.cult[,-1]
+  cultdist <- vegdist(m.cult, method = "jaccard", diag=F, upper=F, na.rm = T)
+  cultdist
+
+# genetic distance
+  m.ibd <- d.genes[, c(2,3,5)]
+  m.ibd<- tidyr::spread(m.ibd, population1, IBDmax)
+  m.ibd <- rbind(c(NA), m.ibd)
+  m.ibd$population2[1] <- "Bafing"
+  rownames(m.ibd) <- m.ibd$population2
+  m.ibd <- m.ibd[,-1]
+  m.ibd <- cbind(m.ibd, c(NA))
+  colnames(m.ibd)[ncol(m.ibd)] <- "Tai"
+  ibddist <- as.dist(m.ibd, diag=F, upper=F)
+  ibddist
+  
+  m.sim <- d.genes[, c(2,3,6)]
+  m.sim<- tidyr::spread(m.sim, population1, similarity)
+  m.sim <- rbind(c(NA), m.sim)
+  m.sim$population2[1] <- "Bafing"
+  rownames(m.sim) <- m.sim$population2
+  m.sim <- m.sim[,-1]
+  m.sim <- cbind(m.sim, c(NA))
+  colnames(m.sim)[ncol(m.sim)] <- "Tai"
+  simdist <- as.dist(m.sim, diag=F, upper=F)
+  simdist
+
+
